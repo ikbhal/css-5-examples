fe79c1dd61d429fa483e0063542ae18f41ce9380
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2022-08-29 14:36:16 +0530

    try grouping, class2, universal

---
+++ src/routes/trycss_grouping/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<h1>Hello World!</h1>
+<h2>Smaller heading!</h2>
+<p>This is a paragraph.</p>
+
+
+<style>
+h1, h2, p {
+  text-align: center;
+  color: red;
+}
+</style>
---
+++ src/routes/trycss_syntax_universal/+page.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<p>Every element on the page will be affected by the style.</p>
+<p id="para1">Me too!</p>
+<p>And me!</p>
+
+<style>
+* {
+  text-align: center;
+  color: blue;
+}
+</style>
